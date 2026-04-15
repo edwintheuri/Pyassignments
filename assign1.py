82572370de41a399531a07530040d3a25006e385
edwintheuri <edwintheuri@gmail.com>
2026-04-15 09:59:34 +0300

    add assign 1

---
+++ assign1.py
@@ -0,0 +1,18 @@
+import math, random 
+#
+num1=input("num1:\n")
+num1
+op = input("num1:\n")
+num2=input("num2:\n")
+if op == "+" :
+    print(num1+num2)
+ 
+
+
+
+#finding a square root of any input
+print(math.sqrt(int(input("Enter a number for Square root : ")))) 
+
+#finding a random number between,including 1 and 100, 1-100
+print(random.randint(1,100))
+
